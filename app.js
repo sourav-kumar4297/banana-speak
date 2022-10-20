ba1a3067438e636a3b0134c4fd17af2af8aa0021
Sourav <souravkumar4297@gmail.com>
2022-10-20 23:43:50 +0530

    taking input and output in console

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-console.log("hello js");
-var username = prompt("Give user name");
+// console.log("hello js");
+//   var username = prompt("Give user name");
 
-var welcomeMessage = "Welcome "+ username;
+//   var welcomeMessage = "Welcome "+ username;
 
-alert(welcomeMessage);
+// alert(welcomeMessage);
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+ 
+btnTranslate.addEventListener("click", clickHandler);
+
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input",txtInput.value);
+}
