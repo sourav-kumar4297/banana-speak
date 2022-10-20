af8da9730d3a9639616c2e78e0a53598771224fb
Sourav <souravkumar4297@gmail.com>
2022-10-21 00:13:42 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// console.log("hello js");
-//   var username = prompt("Give user name");
-
-//   var welcomeMessage = "Welcome "+ username;
-
-// alert(welcomeMessage);
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
- 
-btnTranslate.addEventListener("click", clickHandler);
+ var outputDiv = document.querySelector("#output");
+
+
+
+
+
 
 
 function clickHandler(){
-    console.log("clicked");
-    console.log("input",txtInput.value);
+    outputDiv.innerText="hello beta";
 }
+
+
+btnTranslate.addEventListener("click", clickHandler);
